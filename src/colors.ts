8da6ffed5c119bf75fc575185039f5c4ef2c4ca2
Кирилл Осинцев <osintsev.2000@inbox.ru>
2023-03-18 19:15:17 +0500

    added new colors.

--- src/colors.ts
+++ src/colors.ts
@@ -1,19 +1,20 @@
 export const black = '#252525';
 export const white = '#FFFFFF';
 
-export const gray100 = '#F2F2F2';
-export const gray200 = '#E0E0E0';
-export const gray300 = '#BDBDBD';
-export const gray400 = '#828282';
-export const gray500 = '#4F4F4F';
+export const gray100 = '#F9F9F9';
+export const gray200 = '#F2F2F2';
+export const gray300 = '#E0E0E0';
+export const gray400 = '#BDBDBD';
+export const gray500 = '#828282';
+export const gray600 = '#4F4F4F';
 
 export const red = '#EB5757';
 export const orange = '#F2994A';
 export const yellow = '#F2C94C';
 
 export const green100 = '#6FCF97';
-export const green200 = '#2D9CDB';
-export const green300 = '#2F80ED';
+export const green200 = '#27AE60';
+export const green300 = '#219653';
 
 export const blue100 = '#56CCF2';
 export const blue200 = '#2D9CDB';
